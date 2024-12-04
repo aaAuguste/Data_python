3617671859c7425985a867c07729f2d8e2514843
Aaa2122 <96547921+Aaa2122@users.noreply.github.com>
2024-12-04 13:15:19 +0100

    Ajout map

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,7 +1,8 @@
 from dash import html
-from ..components import histogram
+from ..components import histogram, map
 
 layout = html.Div([
     html.H1("Earthquake Data Dashboard"),
     histogram.histogram_component,
+    map.map_component
 ])
--- src/utils/common_functions.py
+++ src/utils/common_functions.py
@@ -8,3 +8,14 @@ def create_magnitude_histogram(df):
 def load_clean_data():
     df = pd.read_csv('data/cleaned/earthquake_data_cleaned.csv')
     return df
+def create_earthquake_map(df):
+    fig = px.scatter_geo(df,
+                         lat='latitude',
+                         lon='longitude',
+                         hover_name='place',
+                         size='mag',
+                         color='depth',
+                         color_continuous_scale='Viridis',
+                         title='Global Earthquake Locations')
+    fig.update_layout(geo=dict(showland=True))
+    return fig
