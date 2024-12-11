cffe109ff24389902bbaed8554d8bda3c0d259fe
Aaa2122 <96547921+Aaa2122@users.noreply.github.com>
2024-12-11 13:05:27 +0100

    Ajustement taille séisme sur les cartes

--- src/utils/common_functions.py
+++ src/utils/common_functions.py
@@ -38,17 +38,48 @@ def load_clean_data():
 
 
 def create_earthquake_map(df):
+    """
+    Crée une carte interactive des séismes avec des tailles de points adaptées
+    au zoom et à la magnitude.
+    """
+    # Ajustez la taille des points en fonction de la magnitude
+    df['size'] = df['mag'] * 10
+
+    # Créez la carte avec scatter_mapbox
     fig = px.scatter_mapbox(
         df,
         lat="latitude",
         lon="longitude",
-        color="mag",
-        size="mag",
-        hover_name="place",
-        mapbox_style="carto-positron",  # Style de la carte
-        zoom=3
+        size="size",  
+        color="mag",  
+        hover_name="place",  
+        hover_data={"mag": True, "size": False},  
+        mapbox_style="carto-positron",  
+        zoom=1,  
     )
 
+    # Mettez à jour le layout pour activer l'adaptation au zoom
+    fig.update_layout(
+        title="Carte des Séismes",
+        font=dict(family="Arial, sans-serif", size=1),
+        margin=dict(l=0, r=0, t=50, b=0),
+        paper_bgcolor="rgba(0,0,0,0)",
+        mapbox=dict(
+            zoom=1,  
+            center={"lat": df['latitude'].mean(), "lon": df['longitude'].mean()}  
+        )
+    )
+
+    # Ajouter des tailles de point dynamiques basées sur le zoom (optionnelle)
+    fig.update_traces(
+        marker=dict(
+            sizemode='area',
+            sizeref=max(df['size']) / (20.0 ** 2), 
+            sizemin=5  # Taille minimale des points
+        )
+    )
+
+    
     fig.update_layout(
         title="Carte des Séismes",
         font=dict(family="Arial, sans-serif", size=14),
